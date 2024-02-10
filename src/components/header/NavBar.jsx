ce227b0eba5b675acdbae90bcefb9bba006569a4
calindarte <carlosalbertolindartesanto@gmail.com>
2024-02-10 00:19:55 -0500

    menu modificado

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = ({openMenu, setOpenMenu}) => {
 
 
   return (
-    <nav className={`md:static md:flex md:gap-x-8 text-sm font-medium md:mr-10 ${openMenu? 'fixed top-0 left-0 h-full flex flex-col text-sm w-[65%] bg-white px-8 gap-y-8 z-20':'hidden'}`}>
+    <nav className={`md:static md:flex md:gap-x-8 text-sm font-medium md:mr-10 ${openMenu? 'fixed top-0 left-0 h-full flex flex-col text-sm w-[65%] bg-white px-8 gap-y-8 z-30':'hidden'}`}>
         <button className="text-left pt-6 md:hidden" onClick={()=>setOpenMenu(false)}>
           <IconClose className="size-4"/>
         </button>
